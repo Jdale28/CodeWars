b9f6064f886ee404590e988541ea68c37730011a
Jordan Dale <jordan.dale2@gmail.com>
2019-01-29 22:21:54 -0500

    Added final grade challenge

--- index.js
+++ index.js
@@ -108,3 +108,13 @@ function elevator(left, right, call) {
     return "right";
   }
 }
+
+// Student grade
+// https://www.codewars.com/kata/students-final-grade/solutions/javascript
+
+function finalGrade (exam, projects) {
+  if(exam > 90 || projects > 10) return 100;
+  if(exam > 75 & projects >= 5) return 90;
+  if(exam > 50 & projects >= 2) return 75;
+  return 0;
+}
