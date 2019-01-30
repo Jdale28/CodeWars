1042615e2ff0c02e49b74f4b4e435c0fe68371e4
Jordan Dale <jordan.dale2@gmail.com>
2019-01-30 16:43:19 -0500

    Added valid parentheses completed kata

--- index.js
+++ index.js
@@ -237,3 +237,16 @@ function array_diff(a, b) {
    return b.includes(n) === false;
  });
 }
+
+// Valid parenthese
+// https://www.codewars.com/kata/valid-parentheses
+function validParentheses(parens) {
+  var n = 0;
+  for (var i = 0; i < parens.length; i++) {
+    if (parens[i] == "(") n++;
+    if (parens[i] == ")") n--;
+    if (n < 0) return false;
+  }
+
+  return n == 0;
+}
